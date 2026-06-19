4229542bd01d2fb79e474221bea8847e77113958
PabloOjer <ojer.161782@e.unavarra.es>
2026-06-19 14:11:11 +0200

    [rojo] - Crear test que verifica que el comando apostar devuelve string no vacío

--- tests/QuinielaTest.php
+++ tests/QuinielaTest.php
@@ -23,4 +23,18 @@ class QuinielaTest extends TestCase
         // Assert
         $this->assertEquals('', $resultado);
     }
+
+    /** @test */
+    public function reconocerComandoApostarDevuelveStringNoVacio(): void
+    {
+        // Arrange
+        $marcador = $this->createMock(Resultados::class);
+        $quiniela = new Quiniela($marcador);
+
+        // Act
+        $resultado = $quiniela->ejecutar('apostar españa-brasil 1');
+
+        // Assert
+        $this->assertNotEmpty($resultado);
+    }
 }
