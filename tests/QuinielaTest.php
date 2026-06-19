d65f226f213dc940dba500b42bd761429f1896a1
PabloOjer <ojer.161782@e.unavarra.es>
2026-06-19 14:55:53 +0200

    [rojo] - Crear test que verifica que el nombre del partido se normaliza a minúsculas

--- tests/QuinielaTest.php
+++ tests/QuinielaTest.php
@@ -123,4 +123,18 @@ class QuinielaTest extends TestCase
         // Assert
         $this->assertEquals('españa-brasil: 1, francia-alemania: X', $resultado);
     }
+
+    /** @test */
+    public function apostarConNombreDePartidoEnMayusculasLoNormalizaAMinusculas(): void
+    {
+        // Arrange
+        $marcador = $this->createMock(Resultados::class);
+        $quiniela = new Quiniela($marcador);
+
+        // Act
+        $resultado = $quiniela->ejecutar('apostar España-Brasil 1');
+
+        // Assert
+        $this->assertEquals('españa-brasil: 1', $resultado);
+    }
 }
